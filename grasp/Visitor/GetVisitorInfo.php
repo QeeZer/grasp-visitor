8d9e3b0bd8984785f6ca541e52378af3fc9831b1
qeezer <184353381@qq.com>
2019-05-03 18:19:05 +0800

    auto create file

--- grasp/Visitor/GetVisitorInfo.php
+++ grasp/Visitor/GetVisitorInfo.php
@@ -24,7 +24,7 @@ class GetVisitorInfo implements GetVisitorInfoContract
 
 		try {
 			if(!$filesystem->exists($this->file)) {
-				throw new VisitorException($this->file . ' not exists. Please check it.');
+				$filesystem->touch($this->file);
 			}
 
 			$filesystem->chmod($this->file, 0755);
